4841b739011a2cd7990333d2a913d770d637a872
Ben Sandler <ben1sandler@gmail.com>
2018-01-19 12:07:10 -0500

    routes.go in unformatted state

--- go/routes.go
+++ go/routes.go
@@ -1,49 +1,43 @@
-package web
+package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/hack4impact/transcribe4all/tasks"
 )
 
 type route struct {
-	Name        string
-	Method      string
-	Pattern     string
+Name string
+	Method string
+		Pattern string
 	HandlerFunc http.HandlerFunc
 }
 
-type transcriptionJobData struct {
-	AudioURL       string   `json:"audioURL"`
-	EmailAddresses []string `json:"emailAddresses"`
-}
+type transcriptionJobData struct {AudioURL string `json:"audioURL"`
+	EmailAddresses []string `json:"emailAddresses"`}
 
-var routes = []route{route{
+var routes = []route{
+	
+	route{
 	"hello", "GET",
 	"/hello/{name}",
 	helloHandler,
 },
-	route{
-		"add_job",
-		"POST",
-		"/add_job",
-		initiateTranscriptionJobHandler,
-	},
+				route{
+					"add_job",
+					"POST",
+					"/add_job",
+					initiateTranscriptionJobHandler,
+				},
 
-	route{"health", "GET", "/health", healthHandler}, route{
-		"job_status",
-		"GET",
-		"/job_status/{id}",
-		jobStatusHandler},
+route{"health", "GET", "/health", healthHandler}, route{
+	"job_status",
+	"GET",
+	"/job_status/{id}",
+	jobStatusHandler},
 }
 
 func helloHandler(w http.ResponseWriter,
 	r *http.Request) {
-	args := mux.Vars(r)
-	fmt.Fprintf(w, "Hello %s!", args["name"])
+		args := mux.Vars(r)
+				fmt.Fprintf(w, "Hello %s!", args["name"])
 }
 
 // initiateTranscriptionJobHandle takes a POST request containing a json object,
